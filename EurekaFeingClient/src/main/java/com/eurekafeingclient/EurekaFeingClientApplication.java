5038c795b4e471c0e90b1f4f877161da47cd1fe8
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-16 15:07:33 -0500

    .

---
+++ EurekaFeingClient/src/main/java/com/eurekafeingclient/EurekaFeingClientApplication.java
@@ -0,0 +1,13 @@
+package com.eurekafeingclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaFeingClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaFeingClientApplication.class, args);
+	}
+
+}
