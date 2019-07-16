e3a51fac7ef6c6d95b7d135d8c1d48ad1d7855de
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-16 17:05:13 -0500

    ,

--- EurekaFeingClient/src/main/java/com/eurekafeingclient/EurekaFeingClientApplication.java
+++ EurekaFeingClient/src/main/java/com/eurekafeingclient/EurekaFeingClientApplication.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import com.eurekafeingclient.greeting.GreetingClient;
 
 @SpringBootApplication
@@ -25,6 +26,7 @@ public class EurekaFeingClientApplication {
     @RequestMapping("/get-greeting")
     public String greeting(Model model) {
         model.addAttribute("greeting", greetingClient.greeting());
-        return "greeting";
+        return "greeting-view";
     }
+ 
 }
