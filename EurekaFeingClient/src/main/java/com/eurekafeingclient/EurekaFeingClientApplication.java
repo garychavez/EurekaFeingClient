3c10ce82a6b17d1f774a70536010bed144c7019b
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-16 15:52:58 -0500

    .

--- EurekaFeingClient/src/main/java/com/eurekafeingclient/EurekaFeingClientApplication.java
+++ EurekaFeingClient/src/main/java/com/eurekafeingclient/EurekaFeingClientApplication.java
@@ -3,6 +3,7 @@ package com.eurekafeingclient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,6 @@ public class EurekaFeingClientApplication {
     @RequestMapping("/get-greeting")
     public String greeting(Model model) {
         model.addAttribute("greeting", greetingClient.greeting());
-        return "greeting-view";
+        return "greeting";
     }
 }
--- EurekaFeingClient/src/main/java/com/eurekafeingclient/greeting/GreetingClient.java
+++ EurekaFeingClient/src/main/java/com/eurekafeingclient/greeting/GreetingClient.java
@@ -1,5 +1,6 @@
 package com.eurekafeingclient.greeting;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("spring-cloud-eureka-client")
